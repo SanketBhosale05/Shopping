ded4dccf76e9405b261037f21f9cd7d44baec746
91837 <91837@SAHIL>
2023-08-04 11:26:12 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Shopping s1= new Shopping();
+            s1.AcceptDetails(1, "pen", 20, 10, 11);
+          // s1.ShowDetails();
+            s1.CalculateBill();
+        }
+    }
+}
---
+++ Shopping.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping
+{
+    public class Shopping
+    {
+        int Pid;
+        string pname;
+        int pPrice;
+        int qty;
+        int bill;
+
+        public void AcceptDetails(int Pid,string  pname , int pPrice , int qty, int bill)
+        {
+            this.Pid = Pid;
+            this.pname = pname;
+            this.pPrice = pPrice;
+                
+            this.qty = qty;
+            this.bill = bill;
+
+        }
+        public void CalculateBill()
+        {
+            if (this.bill > 0)
+            {
+                bill = qty* pPrice;
+                ShowDetails();
+            }
+            else
+            {
+                Console.WriteLine("qty must more than zero");
+            }
+        }
+        public void ShowDetails()
+        {
+            Console.WriteLine(Pid);
+            Console.WriteLine(pname);
+
+            Console.WriteLine(qty);
+            Console.WriteLine(bill);
+            Console.WriteLine();
+            
+
+        }
+    }
+}
